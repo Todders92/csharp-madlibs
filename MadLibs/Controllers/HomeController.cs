d718b541437e0527d978fcb8bc879f093f5f8168
Todd Walraven and patrick delaney <todd+patrick@>
2020-03-09 11:12:38 -0700

    complete madlibs functionality

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -20,10 +20,16 @@ namespace MadLib.Controllers
     [Route("/form")]
     public ActionResult Form() { return View(); }
 
-    [Route("/funnyform")]
-    public ActionResult FunnyForm()
+    [Route("/madlib")]
+    public ActionResult MadLib(string person1, string person2, string animal, string exclamation, string verb, string noun)
     {
       MadLibVariable newMadLib = new MadLibVariable();
+      newMadLib.Person1 = person1;
+      newMadLib.Person2 = person2;
+      newMadLib.Animal = animal;
+      newMadLib.Exclamation = exclamation;
+      newMadLib.Verb = verb;
+      newMadLib.Noun = noun;
       return View(newMadLib);
     }
 
--- MadLibs/Models/MadLib.Variable.cs
+++ MadLibs/Models/MadLib.Variable.cs
@@ -3,7 +3,7 @@ namespace MadLib.Models
   public class MadLibVariable
   {
     public string Person1 { get; set; }
-    public string  Person2 { get; set; }
+    public string Person2 { get; set; }
     public string Animal { get; set; }
     public string Exclamation { get; set; }
     public string Verb { get; set; }
