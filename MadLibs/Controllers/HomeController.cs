38e247aaad7b2b51385e38c0dd455906e2af4f98
Todd Walraven and patrick delaney <todd+patrick@>
2020-03-09 14:18:16 -0700

    add form for second madlib, refactor constructor to handle more words

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -19,16 +19,55 @@ namespace MadLib.Controllers
     [Route("/form1")]
     public ActionResult Form1() { return View(); }
 
-    [Route("/madlib")]
-    public ActionResult MadLib(string person1, string person2, string animal, string exclamation, string verb, string noun)
+    [Route("form2")]
+    public ActionResult Form2() { return View(); }
+
+    [Route("/madlib1")]
+    public ActionResult MadLib1(string person1, string person2, string animal, string exclamation, string noun1, string noun2, string noun3, string noun4, string noun5, string adjective1, string adjective2, string adjective3, string adjective4, string adjective5, string verb1, string verb2, string adverb1)
+    {
+      MadLibVariable newMadLib = new MadLibVariable();
+
+      newMadLib.Person1 = person1;
+      newMadLib.Person2 = person2;
+      newMadLib.Animal = animal;
+      newMadLib.Exclamation = exclamation;
+      newMadLib.Noun1 = noun1;
+      newMadLib.Noun2 = noun2;
+      newMadLib.Noun3 = noun3;
+      newMadLib.Noun4 = noun4;
+      newMadLib.Noun5 = noun5;
+      newMadLib.Adjective1 = adjective1;
+      newMadLib.Adjective2 = adjective2;
+      newMadLib.Adjective3 = adjective3;
+      newMadLib.Adjective4 = adjective4;
+      newMadLib.Adjective5 = adjective5;
+      newMadLib.Verb1 = verb1;
+      newMadLib.Verb2 = verb2;
+      newMadLib.Adverb1 = adverb1;
+      return View(newMadLib);
+    }
+
+    [Route("/madlib2")]
+    public ActionResult MadLib2(string person1, string person2, string animal, string exclamation, string noun1, string noun2, string noun3, string noun4, string noun5, string adjective1, string adjective2, string adjective3, string adjective4, string adjective5, string verb1, string verb2, string adverb1)
     {
       MadLibVariable newMadLib = new MadLibVariable();
       newMadLib.Person1 = person1;
       newMadLib.Person2 = person2;
       newMadLib.Animal = animal;
       newMadLib.Exclamation = exclamation;
-      newMadLib.Verb = verb;
-      newMadLib.Noun = noun;
+      newMadLib.Noun1 = noun1;
+      newMadLib.Noun2 = noun2;
+      newMadLib.Noun3 = noun3;
+      newMadLib.Noun4 = noun4;
+      newMadLib.Noun5 = noun5;
+      newMadLib.Adjective1 = adjective1;
+      newMadLib.Adjective2 = adjective2;
+      newMadLib.Adjective3 = adjective3;
+      newMadLib.Adjective4 = adjective4;
+      newMadLib.Adjective5 = adjective5;
+      newMadLib.Verb1 = verb1;
+      newMadLib.Verb2 = verb2;
+      newMadLib.Adverb1 = adverb1;
       return View(newMadLib);
     }
 
--- MadLibs/Models/MadLib.Variable.cs
+++ MadLibs/Models/MadLib.Variable.cs
@@ -6,7 +6,18 @@ namespace MadLib.Models
     public string Person2 { get; set; }
     public string Animal { get; set; }
     public string Exclamation { get; set; }
-    public string Verb { get; set; }
-    public string Noun { get; set; } 
+    public string Noun1 { get; set; } 
+    public string Noun2 { get; set; }
+    public string Noun3 { get; set; }
+    public string Noun4 { get; set; }
+    public string Noun5 { get; set; }
+    public string Adjective1 { get; set; }
+    public string Adjective2 { get; set; }
+    public string Adjective3 { get; set; }
+    public string Adjective4 { get; set; }
+    public string Adjective5 { get; set; }
+    public string Verb1 { get; set; }
+    public string Verb2 { get; set; }
+    public string Adverb1 { get; set; }
   }
 }
